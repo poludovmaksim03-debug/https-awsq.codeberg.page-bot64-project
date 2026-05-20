efec61eff057865fb1935972f6881bcd6fe979d0
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-20 18:56:21 +0400

    Delete tesseract.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- tesseract.js
+++
@@ -1,8 +0,0 @@
-const { createWorker } = require('tesseract.js');
-
-(async () => {
-  const worker = await createWorker('eng'); // указание языка
-  const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-  console.log(ret.data.text);
-  await worker.terminate(); // завершение работы с воркеру
-})();
