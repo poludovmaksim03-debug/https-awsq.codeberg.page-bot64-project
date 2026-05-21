528e13ba07b2916060473a825937bf8d6c86dfcf
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-21 23:24:49 +0400

    Update server2.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- server2.js
+++ server2.js
@@ -26,7 +26,7 @@ app.post('/api/yandex-gpt', async (req, res) => {
       return res.status(400).json({ error: 'Некорректные данные запроса' });
     }
 
-    const response = await axios.post('https://ya.ru', {
+    const response = await axios.post('https://ya.ru/', {
       modelUri: `gpt://${YANDEX_FOLDER_ID}/yandexgpt/latest`,
       completionOptions: {
         stream: false,
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
-  console.log('Откройте в браузере: https://ya.ru');
+  console.log('Откройте в браузере: https://ya.ru/');
   console.log('\nДля работы необходимо установить:');
   console.log('- YANDEX_API_KEY в .env файле');
   console.log('- YANDEX_FOLDER_ID в .env файле');
