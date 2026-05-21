8853842d4d81470db414ad7f09cff45210d90113
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-21 21:03:32 +0400

    Update server2.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- server2.js
+++ server2.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
-  console.log('Откройте в браузере: https://poludovmaksim03-debug.github.io/https-awsq.codeberg.page-bot64-project/');
+  console.log('Откройте в браузере: https://ya.ru');
   console.log('\nДля работы необходимо установить:');
   console.log('- YANDEX_API_KEY в .env файле');
   console.log('- YANDEX_FOLDER_ID в .env файле');
