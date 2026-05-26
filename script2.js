14921672300a4372943695219e6a894c02af7619
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-26 10:47:47 +0400

    Update script2.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- script2.js
+++ script2.js
@@ -68,7 +68,7 @@ async recognizeTextFromImage(imageData) {
         this.status('Обращение к Yandex GPT...');
 
         try {
-            const response = await fetch('http://localhost:3000/api/yandex-gpt', {
+            const response = await fetch('http://localhost:8080', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
