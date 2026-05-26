76cbfe5afd4bc927bc2cbbdf4f78deef2b05fa47
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-27 00:22:46 +0400

    Update script2.js
    Signed-off-by: poludovmaksim03-debug <poludovmaksim03@gmail.com>

--- script2.js
+++ script2.js
@@ -68,7 +68,7 @@ async recognizeTextFromImage(imageData) {
         this.status.textContent = 'Обращение к Yandex GPT...';
 
         try {
-            const response = await fetch('http://localhost:5500', {
+            const response = await fetch('https://llm.api.cloud.yandex.net/foundationModels/v1/completion', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
